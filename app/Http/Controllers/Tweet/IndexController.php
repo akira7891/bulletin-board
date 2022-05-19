5b0205e7b7832b7e102ecac98517cd6145a15e61
akira <akiraakira462@gmail.com>
2022-05-20 07:52:21 +0900

    ブレードファイルを作成、コントローラーファイルを修正、データベースのデータをhtmlに出力できました

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Tweet;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return 'シングルアクションコントローラー';
+        $tweets=Tweet::all();
+        return view('tweet.index',['tweets'=>$tweets]);
     }
 }
